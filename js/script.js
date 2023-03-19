a90e707c91e95a5679380f46f088612abfaa7bbd
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-03-19 12:40:02 +0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let tripInfoButton = document.querySelector('.trip-info')
+let dailyBudget = document.querySelector('.daily-budget')
+
+const tripInfo = function () {
+    let totalBudget = Number(prompt("What is your total budget?"))
+    let accommodation = Number(prompt("What are your accommodation cost?"))
+    let numDays = Number(prompt("How many days does your trip last?"))
+
+    calculateDailyBudget(totalBudget, accommodation, numDays)
+}
+
+const calculateDailyBudget = function (totalBudget, accommodation, numDays) {
+    let daily = ((totalBudget - accommodation) / numDays).toFixed(2)
+    dailyBudget.textContent = `You can spend $${daily} a day on food and fun.`
+}
+
+tripInfoButton.onclick = function() {
+    tripInfo()
+}
